803f75f297f52a75db16c1d2aa9598b48e42adfb
Leonardo Reina <leonardo@formahomolog.com.br>
2020-07-23 23:17:10 -0300

    WIP applying incomes and outcomes rule

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,51 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface CreateTransactionDTO {
+  transaction: TransactionDTO;
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions.filter(
+      transaction => transaction.type === 'income',
+    );
+    const outcomes = this.transactions.filter(
+      transaction => transaction.type === 'outcome',
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ transaction, balance }: CreateTransactionDTO): Transaction {
+    const transactionToCreate = new Transaction({
+      title: transaction.title,
+      value: transaction.value,
+      type: transaction.type,
+    });
+
+    this.transactions.push(transactionToCreate);
+    this.balance = balance;
+
+    return transactionToCreate;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,15 +1,57 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionRequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface BalanceRequestDTO {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
+  private balance: BalanceRequestDTO;
+
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
+    this.balance = transactionsRepository.getBalance();
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionRequestDTO): Transaction {
+    if (type === 'outcome' && value > this.balance.total) {
+      throw Error('Outcome value is above the total in the balance');
+    }
+
+    if (type === 'outcome') {
+      this.balance.outcome += value;
+    }
+
+    if (type === 'income') {
+      this.balance.income += value;
+    }
+
+    this.balance.total = this.balance.income - this.balance.outcome;
+
+    const transaction = this.transactionsRepository.create({
+      transaction: {
+        title,
+        value,
+        type,
+      },
+      balance: {
+        income: this.balance.income,
+        outcome: this.balance.outcome,
+        total: this.balance.total,
+      },
+    });
+
+    return transaction;
   }
 }
 
